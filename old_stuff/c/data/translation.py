9eb36cfc4008d69c70b8cc626c5998b5fb773b48
hummelgard <magnus@hummelgard.com>
2016-05-05 09:18:13 +0200

    cleaning up

--- old_stuff/c/data/translation.py
+++
@@ -1,8 +0,0 @@
-from modeltranslation.translator import translator, TranslationOptions
-from .models import Horse
-
-class HorseTranslationOptions(TranslationOptions):
-    fields = ('type', 'gender','color',)
-
-
-translator.register(Horse, HorseTranslationOptions)
--- old_stuff/cloudmare.bak/data/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^add/$', views.addData, name='add data'),
-    url(r'^$', views.index, name='index'),
-]
-
